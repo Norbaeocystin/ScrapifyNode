15c0a977ec10e693d70160c6861f3cc86ede4bf1
Norbaeocystin <baranrastislav@gmail.com>
2020-03-09 19:50:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const emailPattern = /([-a-zA-Z0-9.`?{}]+@[-a-zA-Z0-9.`?{}]+[.\w+]+)/g
 const socialNetworks = ['facebook', 'twitter', 'linkedin', 'instagram']
 const sn = socialNetworks.map(social => social + '[.]com')
 const socialNetworksPattern = new RegExp(sn.join('|'))
-const filter = ['gif$', 'png$', '.peg$', 'jpg$','mp3$', 'example', 'yourname', 'template', 'javascript']
-const filterPattern = new RegExp(filter.join("|"))
+const filter = ['gif$', 'png$', '.peg$', 'jpg$', 'mp3$', 'example', 'yourname', 'template', 'javascript']
+const filterPattern = new RegExp(filter.join('|'))
 
 async function get (url) {
   const response = await request(url)
@@ -15,15 +15,49 @@ async function get (url) {
 async function getEmails (url) {
   const html = await get(url)
   let emails = html.match(emailPattern)
-  if (emails){
+  if (emails) {
     emails = emails.filter(email => !(email.match(filterPattern)))
-    if (emails.length > 0){
-      return Array.from( new Set(emails))
+  }
+  if (!emails) {
+    emails = []
+  }
+  try {
+    let contactUrl = getContactUrl(html)
+    if (contactUrl) {
+      if (!contactUrl.includes('https')) {
+        contactUrl = new URL(contactUrl, url).href
+      }
+      const html2 = await get(contactUrl)
+      let emails2 = html2.match(emailPattern)
+      if (emails2) {
+        emails2 = emails2.filter(email => !(email.match(filterPattern)))
+        if (emails2) {
+          emails.push(...emails2)
+        }
       }
     }
+  } catch (e) {}
+  if (emails) {
+    return Array.from(new Set(emails))
+  }
+}
+
+function getContactUrl (text) {
+  text = text.replace(/'/g, '"')
+  var splits = text.split('<a')
+  if (splits) {
+    splits = splits.map(split => split.split('>')[0])
+    splits = splits.filter(split => split.match('onta'))
+    if (splits) {
+      splits = splits.map(split => split.replace(/'/g, "'").split('href="')[1].split('"')[0])
+      const first = Array.from(new Set(splits)).sort()[0]
+      return first
+    }
+  }
 }
 
 function getSocialNetworksFromHTMLText (text) {
+  text = text.replace(/'/g, '"')
   var splits = text.split('<a')
   let result = {}
   if (splits) {
@@ -31,10 +65,10 @@ function getSocialNetworksFromHTMLText (text) {
     splits = splits.filter(split => split.match(socialNetworksPattern))
     result = {}
     for (const split of splits) {
-      const link = split.split('href="')[1].split('"')[0]
+      const link = split.replace(/'/g, "'").split('href="')[1].split('"')[0]
       for (const socnet of socialNetworks) {
         if (link.includes(socnet)) {
-          result[socnet] = link.split('?')[0]
+          result[socnet] = link
           break
         }
       }
@@ -47,12 +81,31 @@ async function getEmailsAndSNs (url) {
   const html = await get(url)
   let emails = html.match(emailPattern)
   const sns = getSocialNetworksFromHTMLText(html)
-  if (emails){
+  if (emails) {
     emails = emails.filter(email => !(email.match(filterPattern)))
-    if (emails.length > 0){
-      sns.Emails = Array.from( new Set(emails))
+  }
+  if (!emails) {
+    emails = []
+  }
+  try {
+    let contactUrl = getContactUrl(html)
+    if (contactUrl) {
+      if (!contactUrl.includes('https')) {
+        contactUrl = new URL(contactUrl, url).href
+      }
+      const html2 = await get(contactUrl)
+      let emails2 = html2.match(emailPattern)
+      if (emails2) {
+        emails2 = emails2.filter(email => !(email.match(filterPattern)))
+        if (emails2) {
+          emails.push(...emails2)
+        }
       }
     }
+  } catch (e) {}
+  if (emails) {
+    sns.Emails = Array.from(new Set(emails))
+  }
   return sns
 }
 
